6b80fa069996b3cf8a9daa24fdde80b52b74f8a8
Jaya Lekhrajani <jayalekhrajani26@gmail.com>
2017-10-08 05:01:02 +0530

    Add files via upload

---
+++ Assignment 1 PartB - PartC/Models.py
@@ -0,0 +1,9 @@
+
+
+model1 = [32,25,10, 0.01,0.01,0.2,'model1_cifar10.json','model1_cifar10_weights.h5']
+
+model2 = [64,25,10, 0.2,0.01,0.2,'model2_cifar10.json','model2_cifar10_weights.h5']
+
+model3 = [32,20,10, 0.012,0.25,'model3_cifar10.json','model3_cifar10_weights.h5']
+
+
